5f188ab4587738986a79780d925f5bf353c91543
Daniel Mateos <daniel@mateos.cc>
2019-10-17 23:12:01 +1100

    add variable speed to main.py

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ import pygame.display
 import pygame.key
 import pygame.locals
 
+SPEED = 30
 
 controls = {
     "w": "rc_forward",
@@ -16,12 +17,22 @@ controls = {
     "r": "rc_rotateq",
     "f": "rc_up",
     "g": "rc_down",
+    "c": lambda robot, speed: adjust_speed(10),
+    "v": lambda robot, speed: adjust_speed(-10),
     "tab": lambda robot, speed: robot.takeoff(),
     "backspace": lambda robot, speed: robot.land(),
 }
 
 
+def adjust_speed(n: int):
+    global SPEED
+    SPEED += n
+    print("speed is {}".format(SPEED))
+
+
 def run():
+    global SPEED
+
     robot = tello.TelloCommand(3)
 
     pygame.init()
@@ -31,7 +42,8 @@ def run():
     statsdata = udpserver.UDPServer(8890)
     statsdata.recv_start()
 
-    speed = 30
+    videodata = udpserver.UDPServer(11111)
+    videodata.recv_start()
 
     while True:
         time.sleep(0.5)
@@ -45,9 +57,9 @@ def run():
                 if keyname in controls:
                     key_handler = controls[keyname]
                     if type(key_handler) == str:
-                        getattr(robot, key_handler)(speed)
+                        getattr(robot, key_handler)(SPEED)
                     else:
-                        key_handler(robot, speed)
+                        key_handler(robot, SPEED)
             elif e.type == pygame.locals.KEYUP:
                 if keyname in controls:
                     key_handler = controls[keyname]
